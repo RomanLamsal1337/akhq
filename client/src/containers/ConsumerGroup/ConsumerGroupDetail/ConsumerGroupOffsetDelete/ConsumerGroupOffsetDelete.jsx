137a30cd12e8fac957fded148cc783ee584f8336
Ingo Strauch <ingo.strauch@otto.de>
2022-03-17 14:39:19 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- client/src/containers/ConsumerGroup/ConsumerGroupDetail/ConsumerGroupOffsetDelete/ConsumerGroupOffsetDelete.jsx
+++ client/src/containers/ConsumerGroup/ConsumerGroupDetail/ConsumerGroupOffsetDelete/ConsumerGroupOffsetDelete.jsx
@@ -4,48 +4,109 @@ import {
   uriConsumerGroup,
 } from '../../../../utils/endpoints';
 import 'react-toastify/dist/ReactToastify.css';
-import _ from 'lodash';
+import Root from "../../../../components/Root";
+import Table from "../../../../components/Table";
+import constants from "../../../../utils/constants";
+import ConfirmModal from "../../../../components/Modal/ConfirmModal";
 
-class ConsumerGroupOffsetDelete {
+class ConsumerGroupOffsetDelete extends Root {
   state = {
     clusterId: '',
     consumerGroupId: '',
-    topicIds: this.state.topicIds || [],
+    topicIds: [],
+    deleteAllOffsetsForTopic: '',
+    showDeleteModal: false,
+    deleteMessage: '',
   };
 
   schema = {};
 
   componentDidMount() {
-    const { clusterId, consumerGroupId } = this.props.match.params;
+    const {clusterId, consumerGroupId} = this.props.match.params;
 
-    this.setState({ clusterId, consumerGroupId }, () => {
+    this.setState({clusterId, consumerGroupId}, () => {
       this.getTopics();
     });
   }
 
   async getTopics() {
-    const { clusterId, consumerGroupId, topicIds} = this.state;
+    const {clusterId, consumerGroupId} = this.state;
 
     let data = {};
-    if (topicIds === []) {
       data = await this.getApi(uriConsumerGroup(clusterId, consumerGroupId));
       data = data.data;
 
       if (data) {
-        this.setState({ topicIds: _.uniq(data.offsets.map(o => o.topic))});
+        this.setState({topicIds: data.topics.map(topic => ({topic}))});
       } else {
-        this.setState({ topicIds: [] });
+        this.setState({topicIds: []});
       }
-    }
+  }
+
+  showDeleteModal = deleteMessage => {
+    this.setState({ showDeleteModal: true, deleteMessage });
+  };
+
+  closeDeleteModal = () => {
+    this.setState({ showDeleteModal: false, deleteMessage: '' });
+  };
+
+  handleOnDelete(topicId) {
+    this.setState({ deleteAllOffsetsForTopic: topicId }, () => {
+      this.showDeleteModal(
+          <React.Fragment>
+            Do you want to delete all offsets of topic: {<code>{topicId}</code>} ?
+          </React.Fragment>
+      );
+    });
   }
 
   render() {
-    const { consumerGroupId } = this.state;
+    const {consumerGroupId} = this.state;
 
     return (
-      <div>
-        <Header title={`Delete offsets: ${consumerGroupId}`} history={this.props.history} />
-      </div>
+        <div>
+          <div>
+            <Header title={`Delete offsets: ${consumerGroupId}`}
+                    history={this.props.history}/>
+          </div>
+          <div>
+            <Table
+                history={this.props.history}
+                columns={[
+                  {
+                    id: 'topic',
+                    accessor: 'topic',
+                    colName: 'Topic',
+                    type: 'text',
+                    sortable: true
+                  },
+                ]}
+                data={this.state.topicIds}
+                noContent={
+                  <tr>
+                    <td colSpan={3}>
+                      <div className="alert alert-warning mb-0" role="alert">
+                        No offsets found.
+                      </div>
+                    </td>
+                  </tr>
+                }
+                onDelete={(row) => {
+                  this.handleOnDelete(row.topic)
+                }}
+                actions={
+                  [constants.TABLE_DELETE]
+                }
+            />
+          </div>
+          <ConfirmModal
+              show={this.state.showDeleteModal}
+              handleCancel={this.closeDeleteModal}
+              handleConfirm={this.deleteDefinition}
+              message={this.state.deleteMessage}
+          />
+        </div>
     );
   }
 }
